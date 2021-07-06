281374a48248b46b1a67da5dc346e8e93e3379eb
Ramez youakim <ramezyouakimf@gmail.com>
2021-07-06 14:46:19 +0200

    feat(Navigation): HomeScreen added

---
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+interface homeProps {
+
+}
+
+interface homeState {
+
+}
+
+class HomeScreen extends React.PureComponent<homeProps, homeState> {
+
+    constructor(props: homeProps) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <Text>ddddd</Text>
+        )
+    };
+
+}
+export default HomeScreen
