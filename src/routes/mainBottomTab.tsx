208822f17df034d70b005b58b239661316dd775f
Ramez youakim <ramezyouakimf@gmail.com>
2021-07-09 14:51:50 +0200

    feat(settings): settings page added to the main Bottom tab with the langString for localztion

--- src/routes/mainBottomTab.tsx
+++ src/routes/mainBottomTab.tsx
@@ -4,13 +4,16 @@ import {MainBottomTabParamList} from './MainBottomTabParams';
 
 // Screens
 import HomeScreen from '../screens/HomeScreen/HomeScreen';
+import SettingsScreen from '../screens/SettingsScreen/SettingsScreen';
+import localStrings from '../local/main';
 
 const BottomTab = createBottomTabNavigator<MainBottomTabParamList>();
 
 function MainBottomTab() {
   return (
     <BottomTab.Navigator>
-     <BottomTab.Screen name="HomeScreen" options={() => ({ title: "Home" })} component={HomeScreen} />
+      <BottomTab.Screen name="HomeScreen" options={() => ({ title: localStrings.news })} component={HomeScreen}  />
+      <BottomTab.Screen name="SettingsScreen" options={() => ({ title: localStrings.settings })} component={SettingsScreen}/>
     </BottomTab.Navigator>
   );
 }
