f31a98d982ef3d81df5297d508d0cf09f7d80d8f
Ramez youakim <ramezyouakimf@gmail.com>
2021-07-09 14:50:55 +0200

    feat(chore): main reducer added

--- src/reducers/mainReducer.ts
+++ src/reducers/mainReducer.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
-
+import newsSearch from './newsSearch';
+import common from './common';
 
 export default combineReducers({
-
+    newsSearch,
+    common
 });
