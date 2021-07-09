e11e8e0b91c9fe5f0fb584290819e6137ddf6696
Ramez youakim <ramezyouakimf@gmail.com>
2021-07-09 14:30:34 +0200

    feat(lang): set init app lang function to be called on the app start

--- App.tsx
+++ App.tsx
@@ -3,24 +3,25 @@ import { Provider } from 'react-redux';
 import ErrorBoundary from './src/screens/ErrorBoundary/ErrorBoundary';
 import AppNavigator from './src/routes/mainRouter';
 import store from './src/store';
+import { setInitAppLang } from './src/actions/main';
 
 console.disableYellowBox = true;
+setInitAppLang()
 
-class App extends React.Component {
-  render() {
-    return (
+const App = () => {
 
-      <ErrorBoundary>
+  return (
+    <ErrorBoundary>
 
-        <Provider store={store}>
+      <Provider store={store}>
 
-          <AppNavigator />
+        <AppNavigator />
 
-        </Provider>
+      </Provider>
 
-      </ErrorBoundary>
+    </ErrorBoundary>
 
-    )
-  }
+  )
 }
+
 export default App;
