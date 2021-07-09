552eef1c6a176c3ae9c57057f6381d071752690e
Ramez youakim <ramezyouakimf@gmail.com>
2021-07-09 14:33:56 +0200

    feat(chore): api key - api endpoint url added to the config file

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// we must use the ENV for the api key and other info but since 
+// we only have one env i'm just gonna leave it here
+
+export default {
+    apiKey: "2962080114184a07a6dc51348d95b8a4",
+    apiEndpoint: "https://newsapi.org/v2/everything?"
+}
