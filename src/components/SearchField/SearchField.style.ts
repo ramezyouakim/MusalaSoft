16abc060c081e1f4a431d86282b49b322a6cbd28
Ramez youakim <ramezyouakimf@gmail.com>
2021-07-09 14:47:35 +0200

    feat(search): search field component style added

---
+++ src/components/SearchField/SearchField.style.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+import values from '../../modules/values/main';
+
+const style = StyleSheet.create({
+    searchField: {
+        height: 50,
+        margin: values.marginMeduim,
+        padding: values.paddingSmall
+    }
+})
+
+export default style;
